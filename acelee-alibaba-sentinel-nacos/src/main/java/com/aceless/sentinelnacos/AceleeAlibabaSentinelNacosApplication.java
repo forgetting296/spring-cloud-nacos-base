e4f6daf8d8a048371e0122e8d2baf0b47d0db840
liuziping <1131664758@qq.com>
2021-01-18 16:20:17 +0800

    nacos sentinel

---
+++ acelee-alibaba-sentinel-nacos/src/main/java/com/aceless/sentinelnacos/AceleeAlibabaSentinelNacosApplication.java
@@ -0,0 +1,13 @@
+package com.aceless.sentinelnacos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AceleeAlibabaSentinelNacosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AceleeAlibabaSentinelNacosApplication.class, args);
+    }
+
+}
